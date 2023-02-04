a96640d1e2e870f4aea5dbee59f5681c4126b64b
ZeckeyBay <zfatihturan@gmail.com>
2023-02-04 17:00:03 +0300

    portfolio site template for now

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav className="navbar">
+          <div className="nav-title">
+            <h1>My Portfolio</h1>
+          </div>
+          <div className="nav-items">
+            <div className="nav-item">
+              <h2>About Me</h2>
+            </div>
+            <div className="nav-item">
+              <h2>Projects</h2>
+            </div>
+            <div className="nav-item">
+              <h2>Contact</h2>
+            </div>
+          </div>
+        </nav>
       </header>
+      <main>
+        <section className="about-me">
+          <h2>About Me</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+            viverra euismod odio, gravida pellentesque urna varius vitae.
+          </p>
+        </section>
+        <section className="projects">
+          <h2>Projects</h2>
+          <ul>
+            <li>Project 1</li>
+            <li>Project 2</li>
+            <li>Project 3</li>
+          </ul>
+        </section>
+        <section className="contact">
+          <h2>Contact</h2>
+          <p>
+            Email: <a href="mailto:email@example.com">email@example.com</a>
+          </p>
+          <p>
+            Phone: <a href="tel:+1234567890">(123) 456-7890</a>
+          </p>
+        </section>
+      </main>
+      <footer>
+        <p>Copyright &copy; {new Date().getFullYear()}</p>
+      </footer>
     </div>
   );
 }
